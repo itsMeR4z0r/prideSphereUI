91568e9475b954b74b645f7cbc45c78100359c3f
Isaac Pinheiro <isaacpinheiro@icloud.com>
2023-10-18 16:58:30 -0300

    - menu dashboard

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -1,9 +1,40 @@
+function removeTooltip() {
+    const tooltips = document.querySelectorAll('[role="tooltip"]');
+    for (let tooltip of tooltips) {
+        tooltip.remove();
+    }
+    const elementosTooltip = document.querySelectorAll('[aria-describedby*="tooltip"]');
+
+    elementosTooltip.forEach(elemento => {
+        elemento.removeAttribute('aria-describedby');
+    });
+}
 $(document).ready(function () {
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+    for (let tooltip of tooltipList) {
+        tooltip._config.offset = [0, 16];
+    }
+    window.addEventListener("resize", ()=>{
+        const span = document.querySelector('span.d-none');
+        if (span) {
+            if (span.checkVisibility()) {
+                for (let tooltip of tooltipList) {
+                    tooltip.disable();
+                }
+            }else{
+                for (let tooltip of tooltipList) {
+                    tooltip.enable();
+                }
+            }
+        }
+    });
     const collapseElementList = document.querySelectorAll('.collapse');
     for (let collapseEl of collapseElementList) {
         collapseEl.addEventListener('show.bs.collapse', function () {
             const link = document.querySelector('[href="#' + collapseEl.id + '"]');
             const icon = link.querySelector('.collapse-icon');
+            removeTooltip();
             if (icon) {
                 $(icon).animate({deg: 90}, {
                     duration: 250, step: function (now) {
@@ -23,9 +54,6 @@ $(document).ready(function () {
                     }
                 });
             }
-
         });
     }
-    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 });
