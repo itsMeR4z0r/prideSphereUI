cc0a42ab78d374c29ccbb033838371ff877a0da4
Isaac Pinheiro <isaacpinheiro@icloud.com>
2023-11-21 19:50:44 -0300

    Update map initialization to support specific coordinates and refactor index route to handle lat and lon query parameters

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,17 +1,17 @@
 let map = null;
 mapboxgl.accessToken = 'pk.eyJ1IjoiaXNhYWMyMDgxIiwiYSI6ImNqdzBvaDk1MzAyNTA0OG1teWVhZHI1MncifQ.Heg3a17kDWdwNSjZcMOZGQ';
 $(document).ready(function () {
+
+
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
             map = new mapboxgl.Map({
                 container: 'map',
-                style: 'mapbox://styles/mapbox/streets-v12',
-                center: [position.coords.longitude, position.coords.latitude],
+                style: 'mapbox://styles/isaac2081/clp8ag0r000yo01nwe2mx402n',
+                center: [lon == 0 ? position.coords.longitude : lon, lat == 0 ? position.coords.latitude : lat],
                 zoom: 15,
                 trackUserLocation: true
             });
-
-            map.addControl(new mapboxgl.NavigationControl());
 
             let geolocate = new mapboxgl.GeolocateControl({
                 positionOptions: {
@@ -20,18 +20,33 @@ $(document).ready(function () {
                 trackUserLocation: true, // Draw an arrow next to the location dot to indicate which direction the device is heading.
                 showUserHeading: true
             });
-
             map.addControl(geolocate);
-
+            map.addControl(new mapboxgl.NavigationControl());
             map.on('load', function () {
-                geolocate.trigger();
+                if (lon == 0 && lat == 0) {
+                    geolocate.trigger();
+                }
             });
         });
     } else {
         map = new mapboxgl.Map({
-            container: 'map', style: 'mapbox://styles/mapbox/streets-v12', zoom: 14
+            container: 'map', style: 'mapbox://styles/isaac2081/clp8ag0r000yo01nwe2mx402n', zoom: 14, center: [lon, lat]
+
         });
+        let geolocate = new mapboxgl.GeolocateControl({
+            positionOptions: {
+                enableHighAccuracy: true
+            }, // When active the map will receive updates to the device's location as it changes.
+            trackUserLocation: true, // Draw an arrow next to the location dot to indicate which direction the device is heading.
+            showUserHeading: true
+        });
+        map.addControl(geolocate);
         map.addControl(new mapboxgl.NavigationControl());
+        map.on('load', function () {
+            if (lon == 0 && lat == 0) {
+                geolocate.trigger();
+            }
+        });
     }
 
 });
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    let lat = 0;
+    let lon = 0;
+    if (req.query.lat && req.query.lon) {
+        lat = req.query.lat;
+        lon = req.query.lon;
+    }
+    res.render('index', {title: 'Express',lat: lat, lon: lon});
 });
 
 module.exports = router;
